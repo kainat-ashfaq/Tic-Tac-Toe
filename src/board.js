341eff2dd188e0bdcee238a38a410a627d22eee7
= <=>
2024-07-19 23:06:36 +0500

    3 commit

--- src/board.js
+++ src/board.js
@@ -1,10 +1,20 @@
+var game_board = document.querySelector(".main-board-div");
 var box = document.querySelectorAll(".box")
 var msg = document.querySelector(".msg")
 var resetBtn = document.getElementById("reset");
 var new_Game = document.getElementById("new-game");
 var turn = document.querySelector(".turn")
+var getData = JSON.parse(localStorage.getItem("game_sign"))
+var congratulations = document.getElementById("congratulations");
+var funnyCat = document.querySelector(".funnyCat")
+var thumbs_up = document.querySelector(".thumbs-up");
+var confetti = document.querySelector(".confetti")
+var player_one = getData.player1
+// console.log(player_one.value);
+var player_two = getData.player2
 var game_Over = false
 
+// game winning patterns 
 var player1 = true
 var winPatterns = [
     [0,1,2],
@@ -17,15 +27,16 @@ var winPatterns = [
     [6,7,8],
 ]
 
+// for show text in box 
 box.forEach(function(box){
     box.addEventListener("click", function(){
         if(player1){
-          box.innerText = "o"
-          turn.innerText = "player x's turn"
+          box.innerText = `${player_one}`
+          turn.innerText = `player ${player_two}'s turn`
           player1 = false
         }else{
-            box.innerText = "x"
-          turn.innerText = "player o's turn";
+            box.innerText = `${player_two}`
+          turn.innerText = `player ${player_one}'s turn`;
 
             player1 = true
         }
@@ -34,6 +45,7 @@ box.forEach(function(box){
         checkDraw()
     })
 })
+// check the winner 
 const checkWinner = () => {
   for (let patterns of winPatterns) {
     var position1 = box[patterns[0]].innerText;
@@ -48,11 +60,20 @@ const checkWinner = () => {
 
   }
 };
+// show who is winner 
 const showWinner = (winner) => {
-  msg.innerText = `winner is ${winner} `;
+  congratulations.innerText = "Congratulations";
+  msg.innerHTML = ` winner is ${winner} `;
   turn.innerText = ""
+  game_board.style.display = "none"
+  new_Game.style.display = "block"
+  new_Game.innerText = "New Game"
+  resetBtn.style.display = "none"
+  thumbs_up.style.display = "block"
+  confetti.style.display = "block"
   disabledBox();
 };
+// check match is draw or not 
 const checkDraw = ()=>{
     if(!game_Over ){
         let isDraw = true
@@ -61,15 +82,26 @@ const checkDraw = ()=>{
         })
         if(isDraw ){
             game_Over = true
-            msg.innerText = "It's a draw!";
+            msg.innerText = "Tie game! Good efforts from both sides, it's a draw!";
+            turn.innerText = ""
+            new_Game.innerText = "Play Again!"
+            congratulations.innerText = ""
+            game_board.style.display = "none"
+            funnyCat.style.display = "block"
+            new_Game.style.display = "block"
+            resetBtn.style.display = 'none'
+            confetti.style.display = "none"
+
         }
     }
 }
+// after winning the match box is disabled 
 const disabledBox =() =>{
     for (const boxes of box) {
         boxes.disabled = true
     }
 }
+// for enableBox on reset btn 
 const enableBox = () => {
   for (let boxes of box) {
     boxes.disabled = false;
@@ -79,11 +111,22 @@ turn.innerText = ""
 
   }
 };
+// for clear the board 
 const reset = () =>{
 player1 = true
 enableBox();
 
 }
-// msg.style.display = "none"; 
-new_Game.addEventListener("click" , reset)
+new_Game.addEventListener("click",function(){
+  reset()
+  game_board.style.display = "block"
+  resetBtn.style.display = "block"
+  congratulations.innerText = ""
+  new_Game.style.display = "none"
+  funnyCat.style.display = "none"
+  thumbs_up.style.display = "none"
+})
+// new_Game.addEventListener("click" , reset)
 resetBtn.addEventListener("click" , reset)
+
+
